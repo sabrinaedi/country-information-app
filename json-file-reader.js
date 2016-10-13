e986800984ed6f14050655c5a457cd780e16e52d
Sabedi <nsab@edinger.org>
2016-10-13 11:13:11 +0200

    polished and commented on the files

--- json-file-reader.js
+++ json-file-reader.js
@@ -1,23 +1,13 @@
-
+// import file system to use readFile function and parse
 var fs = require ('fs')
 
-
+//create function that reads and parses a given file, including a callback from the nodeapp.js file (the filename and print function are thus added through nodeapp.js)
 function readMe ( filename, callback ) {
-
 	fs.readFile( filename, 'utf8', function(err, data) {
 		if (err) throw err
-		callback ( JSON.parse(data) )
+			callback ( JSON.parse(data) )
 	})
 }
 
-
-
-
-//for loop runs through the objects in the string and identifies the object the value of which matches the given x (which is the value matching the "names" key)
-//loop prints the name of the country and top Level Domain
-	
-
-
-
-
+// export the readme function, thereby it is made available for nodeapp.js
 module.exports = readMe
--- nodeapp.js
+++ nodeapp.js
@@ -1,39 +1,20 @@
-
+// import file system and the json-file-reader to use in this file
 var fs = require ('fs')
 var readMe = require ('./json-file-reader')
 
-// command line: node nodefile.js Netherlands
-
-// Take in a parameter from the command line
-
+// Take in a parameter from the command line and call function
 readCountry(process.argv[2])
 
-
-// Use readfile and json.parse to read and parse the countries.json file
+// function uses the readMe function defined in the json-file reader
+// for loop runs through the objects in the string and identifies the object the value of which matches the given x (which is the value matching the "names" key)
+// loop prints the name of the country and top Level Domain
 function readCountry ( x ) {
-//for loop runs through the objects in the string and identifies the object the value of which matches the given x (which is the value matching the "names" key)
-//loop prints the name of the country and top Level Domain
-		
-		readMe(__dirname + '/countries.json', function ( array ) {
-
-
+	readMe(__dirname + '/countries.json', function ( array ) {
 		for (var i=0; i<array.length; i++) {
-
 			if (array[i].name == x) {
 				console.log("Country: " + array[i].name )
-				console.log("Top Level Domain: " + array[i].topLevelDomain)
+				console.log("Top Level Domain: " + array[i].topLevelDomain )
 			}
 		}
-
 	})
 }
-
-
-
-// [{ "name":"Netherlands", "topLevelDomain": [".nl"]} ]
-
-/// MODIFY TO INCLUDE NON-CAPITALISED WORDS
-
-
-// return readM
-
